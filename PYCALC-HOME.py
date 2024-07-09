fcb6ab7e158f389146ff0c2296ba35037633d1dc
Student 1 <student@huntscholars.co.uk>
2024-07-09 14:05:02 +0100

    Below Line 60 Needs writing

--- PYCALC-HOME.py
+++ PYCALC-HOME.py
@@ -26,6 +26,16 @@ def BoatParser(BoatData):
     return BoatList
 
 
+# Define the GetAdjustedTime function.
+def GetAdjustedTime(py, Elasted_time):
+    return Elasted_time * 1000 / py
+
+def BoatPrinter(BoatList):
+    for i in range(0, len(BoatList) - 1, 2):
+        print(f"Adjusted time for the {0} is approximately {1:.2f} seconds", BoatList[i], BoatList[i + 1])
+
+
+
 # Get the current time
 startime = datetime.now() # 20
 
@@ -41,12 +51,15 @@ time_difference = datetime.now() - startime
 # Print the time difference in seconds
 print(f"The race has been going on for approximately {time_difference.total_seconds():.2f} seconds")
 
-# Define the GetAdjustedTime function.
-def GetAdjustedTime(py, Elasted_time):
-    return Elasted_time * 1000 / py
+
+with open("PYLIST.txt", "r") as MyFile:
+    BoatFile = MyFile.read()
+BoatList = BoatParser(BoatFile)
 
 # Boats
 py_table = {"ILCA 6": 1154, "ILCA 7": 1102}
+for i in range(1, len(BoatList) - 1, 2):
+    GetAdjustedTime(BoatList[i], time_difference)
 
 
 # Calculate the adjusted times for boats (ILCA 6 and ILCA 7)
@@ -58,12 +71,9 @@ adjus_time_seconds = adjus_time.total_seconds()
 adjus_time1_seconds = adjus_time1.total_seconds()
 
 # Print the adjusted times in seconds
-print(f"Adjusted time for the ILCA 6 is approximately {adjus_time_seconds:.0f} seconds")
-print(f"Adjusted time for the ILCA 7 is approximately {adjus_time1_seconds:.0f} seconds")
-
 # Calculate the difference needed for the ILCA 7 to beat the ILCA 6
 time_difference_seconds = adjus_time1.total_seconds() - adjus_time.total_seconds()
 
 # Print the difference needed for the ILCA 7 to beat the ILCA 6
 if time_difference_seconds > 0:
-    print(f"The ILCA 7 needs to finish ahead by approximately {time_difference_seconds:.0f} seconds to win.")
+    print(f"The boat needs to finish ahead by approximately {time_difference_seconds:.0f} seconds to win.")
