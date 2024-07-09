8267de63a738dbc396e89f1f307876ff6567902e
Student 1 <student@huntscholars.co.uk>
2024-07-09 14:34:46 +0100

    Abandon hope all ye who enter here.

--- PYCALC-HOME.py
+++ PYCALC-HOME.py
@@ -18,7 +18,7 @@ def BoatParser(BoatData):
             if BoatData[i] == "\n":
                 readingName = True
                 BoatList.append(int(tempNum))
-                print(BoatList)
+                #rint(BoatList)
                 tempNum = ""
             else:
                 tempNum = tempNum + BoatData[i]
@@ -57,23 +57,24 @@ with open("PYLIST.txt", "r") as MyFile:
 BoatList = BoatParser(BoatFile)
 
 # Boats
-py_table = {"ILCA 6": 1154, "ILCA 7": 1102}
-for i in range(1, len(BoatList) - 1, 2):
-    GetAdjustedTime(BoatList[i], time_difference)
-
+NewBoatList = []
+#for i in range(1, len(BoatList) - 1, 2):
+#    NewBoatList.append(GetAdjustedTime(BoatList[i], time_difference).total_seconds())
+NewBoatList.append(GetAdjustedTime(BoatList[1], time_difference).total_seconds())
+NewBoatList.append(GetAdjustedTime(BoatList[3], time_difference).total_seconds())
 
 # Calculate the adjusted times for boats (ILCA 6 and ILCA 7)
-adjus_time = GetAdjustedTime(py_table["ILCA 6"], time_difference)
-adjus_time1 = GetAdjustedTime(py_table["ILCA 7"], time_difference)
+"""adjus_time = GetAdjustedTime(py_table["ILCA 6"], time_difference)
+adjus_time1 = GetAdjustedTime(py_table["ILCA 7"], time_difference)"""
 
 # Convert the timedelta objects to seconds
-adjus_time_seconds = adjus_time.total_seconds()
-adjus_time1_seconds = adjus_time1.total_seconds()
+#adjus_time_seconds = adjus_time.total_seconds()
+#adjus_time1_seconds = adjus_time1.total_seconds()
 
 # Print the adjusted times in seconds
 # Calculate the difference needed for the ILCA 7 to beat the ILCA 6
-time_difference_seconds = adjus_time1.total_seconds() - adjus_time.total_seconds()
-
+time_difference_seconds = NewBoatList[1] - NewBoatList[0]
+print(time_difference_seconds, NewBoatList)
 # Print the difference needed for the ILCA 7 to beat the ILCA 6
 if time_difference_seconds > 0:
     print(f"The boat needs to finish ahead by approximately {time_difference_seconds:.0f} seconds to win.")
